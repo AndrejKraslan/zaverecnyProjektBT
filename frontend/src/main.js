2debab29c8e490cd8628be7088c2b00a3d64eaae
JakubKrakovik <jakubkrakovik@gmail.com>
2024-05-21 13:38:50 +0200

    Navbar + router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import axios from './plugins/axios.js';
 
+import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(App);
 
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.use(BootstrapVue3);
+app.mount('#app');
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
+    <p>Test</p>
     <TheWelcome />
   </main>
 </template>
+
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+</script>
